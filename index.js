c8c0c9f3624d803dec266b48136cf46ef3961abb
Andrew Walters <andrewjpwalters@gmail.com>
2022-04-20 21:16:24 -0500

    commit tree fa124e7fc89ab7780b8576474ae3b62f5fb3d0fa

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
   Add your pseudocode here
+  for loop checking each number
+    nested for loop checking the rest of the numbers
+      return checking if sum of numbers equals target input
 */
 
 /*
   Add written explanation of your solution here
+  Using nested loops, the function checks each number against all
+  the following numbers, one by one, until the target number is found
+  and true is returned. If it is not, then false is returned. 
 */
 
 // You can run `node index.js` to view these console logs
